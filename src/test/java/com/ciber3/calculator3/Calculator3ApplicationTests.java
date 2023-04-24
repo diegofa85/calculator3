78dfb97c01b1dc1966e74c5fede3356f5804d41f
diegofa85 <diegofa85@educastur.es>
2023-04-24 16:33:45 +0200

    Añadido esqueleto Spring Boot

---
+++ src/test/java/com/ciber3/calculator3/Calculator3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciber3.calculator3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
